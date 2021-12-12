fdb997af5ef713f1ef82c4dc7fbe815acd935386
Illensky <alexis.laroche.02240@gmail.com>
2021-12-12 19:36:13 +0100

    V1 done

--- assets/JS/countdown.js
+++ assets/JS/countdown.js
@@ -1,16 +1,27 @@
-const timer = function (startBtn, pauseBtn, resetBtn, div, seconds, minutes, hours) {
+const timer = function (form, setBtn, startBtn, pauseBtn, resetBtn, div, seconds, minutes, hours) {
 
+    const alarm = new Audio("assets/sound/joke_drum_effect.mp3")
     let timeOutId = null;
 
     this.draw = function () {
-        if (seconds === 60) {
-            div.innerHTML = hours.toString() + " : 0" + minutes.toString() + " : 00";
+        form.style.display = "none";
+        setBtn.style.display = "none";
+        startBtn.style.display = "inline";
+        pauseBtn.style.display = "inline";
+        resetBtn.style.display = "inline";
+
+        if (hours === 0 && minutes === 60) {
+            div.innerHTML = "1 : 0 : 0" + seconds.toString();
         }
-        else if (minutes === 60 ) {
-            div.innerHTML = hours.toString() + " : 00" + " : " + seconds.toString();
+
+        else if (seconds === 60) {
+            div.innerHTML = hours.toString() + " : 0" + minutes.toString() + " : 00";
         }
         else if (minutes === 60 && seconds === 60) {
             div.innerHTML = hours.toString() + " : 00" + " : 00";
+        }
+        else if (minutes === 60 ) {
+            div.innerHTML = hours.toString() + " : 00" + " : " + seconds.toString();
         }
         else if (seconds.toString().length === 1 && minutes.toString().length > 1) {
             div.innerHTML = hours.toString() + " : " + minutes.toString() + " : 0" + seconds.toString();
@@ -29,14 +40,18 @@ const timer = function (startBtn, pauseBtn, resetBtn, div, seconds, minutes, hou
     this.countdown = function () {
         timeOutId = setInterval(() => {
             if (seconds >= 0 && minutes >= 0 && hours >= 0) {
-                if (seconds === 60) {
-                    div.innerHTML = hours.toString() + " : 0" + minutes.toString() + " : 00";
+                if (hours === 0 && minutes === 60) {
+                    div.innerHTML = "1 : 0 : 0" + seconds.toString();
                 }
-                else if (minutes === 60 ) {
-                    div.innerHTML = hours.toString() + " : 00" + " : " + seconds.toString();
+
+                else if (seconds === 60) {
+                    div.innerHTML = hours.toString() + " : 0" + minutes.toString() + " : 00";
                 }
                 else if (minutes === 60 && seconds === 60) {
                     div.innerHTML = hours.toString() + " : 00" + " : 00";
+                }
+                else if (minutes === 60 ) {
+                    div.innerHTML = hours.toString() + " : 00" + " : " + seconds.toString();
                 }
                 else if (seconds.toString().length === 1 && minutes.toString().length > 1) {
                     div.innerHTML = hours.toString() + " : " + minutes.toString() + " : 0" + seconds.toString();
@@ -60,6 +75,16 @@ const timer = function (startBtn, pauseBtn, resetBtn, div, seconds, minutes, hou
                     minutes = 60;
                 }
             }
+            else{
+                seconds = 0;
+                minutes = 0;
+                hours = 0;
+                clearTimeout(timeOutId);
+                startBtn.style.display = "none";
+                pauseBtn.style.display = "none";
+                div.innerHTML = "Fini !!";
+                alarm.play()
+            }
         }, 1000)
     }
 
@@ -73,7 +98,12 @@ const timer = function (startBtn, pauseBtn, resetBtn, div, seconds, minutes, hou
             minutes = 0;
             hours = 0;
             clearTimeout(timeOutId)
-            div.innerHTML = hours.toString() + " : " + minutes.toString() + " : " + seconds.toString();
+            div.innerHTML = "";
+            form.style.display = "inline";
+            setBtn.style.display = "inline";
+            startBtn.style.display = "none";
+            pauseBtn.style.display = "none";
+            resetBtn.style.display = "none";
         })
     }
 }
@@ -83,24 +113,37 @@ const stop = document.querySelector("#stop");
 const pause = document.querySelector("#pause");
 const count = document.querySelector("#countDiv");
 const set = document.querySelector("#set");
+const form = document.querySelector("#timerForm")
 
-let userHours;
-let userMinutes;
-let userSeconds;
+let userHours = document.querySelector("#hours");
+let userMinutes = document.querySelector("#minutes");
+let userSeconds = document.querySelector("#seconds");
 
 set.addEventListener("click", () => {
-    userHours = parseInt(document.querySelector("#hours").value);
-    userMinutes = parseInt(document.querySelector("#minutes").value);
-    userSeconds = parseInt(document.querySelector("#seconds").value);
+    userHours = document.querySelector("#hours");
+    userMinutes = document.querySelector("#minutes");
+    userSeconds = document.querySelector("#seconds");
+    if (userSeconds.value.length > 0) {
+        if (userHours.value.length > 0 ){
+            userHours = parseInt(document.querySelector("#hours").value);
+        }
+        else {
+            userHours = 0;
+        }
 
-    const testTimer = new timer(start, pause, stop, count, userSeconds, userMinutes, userHours);
+        if (userMinutes.value.length > 0 ) {
+            userMinutes = parseInt(document.querySelector("#minutes").value);
+        }
+        else {
+            userMinutes = 0;
+        }
 
-    testTimer.draw()
-    testTimer.setCountdown()
+        userSeconds = parseInt(document.querySelector("#seconds").value);
+
+        const testTimer = new timer(form, set, start, pause, stop, count, userSeconds, userMinutes, userHours);
+
+        testTimer.draw()
+        testTimer.setCountdown()
+    }
+
 })
-
-
-
-
-
-
